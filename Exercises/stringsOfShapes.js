4b06f6cfd980450f6ad4ce4be16628097cdd0413
Delphine  Dara <delphine.dara@gmail.com>
2021-12-27 21:10:51 -0600

    Completed exercises from chapter 10 - strings of shapes

--- Exercises/stringsOfShapes.js
+++ Exercises/stringsOfShapes.js
@@ -1,4 +1,4 @@
-//Line
+// Stings of shapes
 
 function makeLine(size){
     let line ='';
@@ -13,7 +13,7 @@ function makeSquare(size){
     for(let i=0; i<size; i++){
         square += "\n" + makeLine(size);
     }
-    return square;
+    return square.slice(0, -1);
 }
 
 function makeRectangle(width,height){
@@ -21,7 +21,47 @@ function makeRectangle(width,height){
     for (let i=0; i<height; i++){
         rectangle+=(makeLine(width) + '\n');
     }
-    return rectangle;
+    return rectangle.slice(0,-1);
+}
+
+function makeDownwardStairs(height){
+
+    let downwardStairs ='';
+    for (let i=0; i<height; i++){
+        downwardStairs+=(makeLine(i) +'\n');
+    }
+    return downwardStairs.slice(0,-1);
+}
+
+function makeSpaceLine(numSpaces,numChars){
+    let space='';
+    for (let i=0; i< numSpaces; i++){
+        space+=' ';
+    }
+    return space +makeLine(numChars) + space;
+}
+
+function makeIsoscelesTriangale(height){
+    let isoscelesTriangle='';
+    for (let i=0; i< height; i++){
+        isoscelesTriangle +=( makeSpaceLine(height-i,i) + '\n');
+    }
+
+    return isoscelesTriangle;
+
 }
 
 
+function makeDiamond(height) {
+    let topDiamond = makeIsoscelesTriangale(height);
+    let bottomDiamond = '';
+  
+    for (let i = 0; i <= height; i++) {
+      bottomDiamond += (makeSpaceLine(i, height - i) + '\n');
+    }
+  
+  
+  
+    return topDiamond.slice(0, -1) + '\n' + bottomDiamond.slice(0, -1);
+  }
+  console.log(makeDiamond(9));
