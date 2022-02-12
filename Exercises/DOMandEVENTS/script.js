c8f876d11ee94b754c301d7aaa6bcd22348a30bc
Delphine  Dara <delphine.dara@gmail.com>
2022-02-11 18:14:00 -0600

    minor change

--- Exercises/DOMandEVENTS/script.js
+++ Exercises/DOMandEVENTS/script.js
@@ -1,28 +1,28 @@
-function init () {
+function init() {
     const missionAbort = document.getElementById("abortMission");
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
-
-    button.addEventListener('click', event =>{
-        paragraph.innerHTML = 'Houston! We have liftoff!';
+  
+    button.addEventListener('click', event => {
+      paragraph.innerHTML = 'Houston! We have liftoff!';
     });
-
-    missionAbort.addEventListener("mouseover", event =>{
-        event.target.style.backgroundColor ="red";
+  
+    missionAbort.addEventListener("mouseover", event => {
+      event.target.style.backgroundColor = "red";
     });
-
-    missionAbort.addEventListener("mouseout", event =>{
-        event.target.style.backgroundColor ="";
-
+  
+    missionAbort.addEventListener("mouseout", event => {
+      event.target.style.backgroundColor = "";
+  
     });
-
-    missionAbort.addEventListener("click", event =>{
-        let response = window.confirm (" Are you sure you want to abort the mission?");
-        if (response){
-            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home';
-        }
+  
+    missionAbort.addEventListener("click", event => {
+      let response = window.confirm(" Are you sure you want to abort the mission?");
+      if (response) {
+        paragraph.innerHTML = 'Mission aborted! Space shuttle returning home';
+      }
     });
-
-}
-
-window.addEventListener("load", init);
+  
+  }
+  
+  window.addEventListener("load", init);
