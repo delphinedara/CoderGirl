ddad39739b8df78b45ecf2b0bbac5a4541cb8788
Delphine  Dara <delphine.dara@gmail.com>
2022-02-27 13:55:18 -0600

    Added DOM and Events Execercise

--- Exercises/DOMandEVENTS/script.js
+++ Exercises/DOMandEVENTS/script.js
@@ -2,7 +2,9 @@ function init() {
     const missionAbort = document.getElementById("abortMission");
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
-  
+
+
+   
     button.addEventListener('click', event => {
       paragraph.innerHTML = 'Houston! We have liftoff!';
     });
