21588d6aa12f32d2deb570f643841ca8854ee30d
Delphine  Dara <delphine.dara@gmail.com>
2022-03-15 21:07:48 -0500

    code refactored

---
+++ CypressAutomation/cypress/integration/examples/test1.js
@@ -0,0 +1,9 @@
+describe ('Test Suite Example 1', function(){
+it ('First Test Case', function(){
+
+    cy.visit("https://lets-travel-d.herokuapp.com/");
+
+
+})
+
+})
---
+++ JavaScript/Angular/angular_practice/mission-planner/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CrewComponent } from './crew/crew.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { ExperimentsComponent } from './experiments/experiments.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CrewComponent,
+    EquipmentComponent,
+    ExperimentsComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
