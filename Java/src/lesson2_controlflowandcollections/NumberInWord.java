e56169321448d15a62fac7bdc7e6e11a10a5b856
Delphine  Dara <delphine.dara@gmail.com>
2022-06-02 21:21:33 -0500

    exercise

---
+++ Java/src/lesson2_controlflowandcollections/NumberInWord.java
@@ -0,0 +1,12 @@
+package lesson2_controlflowandcollections;
+
+public class NumberInWord {
+    public static void main(String[] args){
+        System.out.println("To complete later!");
+    }
+
+
+
+
+
+}
